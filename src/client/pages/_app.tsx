1545d87efa1eb2805a4d4bdad3c44d0babeef63e
masasami <masangular0518@gmail.com>
2021-07-18 00:36:19 +0900

    コンポーネントの読み込みパス修正（自作フォルダは相対パスじゃないとダメ？）

--- src/client/pages/_app.tsx
+++ src/client/pages/_app.tsx
@@ -1,7 +1,7 @@
 import { NextPageContext } from 'next'
 import { AppProps } from 'next/app'
 
-import Layout from 'components/Layout'
+import Layout from '../components/Layout'
 import 'styles/_app.scss'
 
 export default function MyApp({ Component, pageProps }: AppProps, ctx: NextPageContext) {
