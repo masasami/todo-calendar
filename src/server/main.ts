ae8c2825a65bbdaa3b6e085d8c0f556ea376ac83
masasami <masangular0518@gmail.com>
2021-08-09 03:29:47 +0900

    環境変数の確認

--- src/server/main.ts
+++ src/server/main.ts
@@ -2,6 +2,12 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
+  console.log(process.env.DB_TYPE)
+  console.log(process.env.DB_HOST)
+  console.log(process.env.DB_PORT)
+  console.log(process.env.DB_USERNAME)
+  console.log(process.env.DB_PASSWORD)
+  console.log(process.env.DATABASE)
   const app = await NestFactory.create(AppModule)
   await app.listen(process.env.PORT || 3000)
 }
