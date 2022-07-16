60349e7fc40189684b9acf87ba4a182a7e701256
masasami <masavel0518@gmail.com>
2022-07-16 10:12:52 +0900

    DBへの接続方法を修正

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,12 +3,6 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
   console.log(process.env.NODE_ENV)
-  console.log(process.env.DB_TYPE)
-  console.log(process.env.DB_HOST)
-  console.log(process.env.DB_PORT)
-  console.log(process.env.DB_USERNAME)
-  console.log(process.env.DB_PASSWORD)
-  console.log(process.env.DATABASE)
   const app = await NestFactory.create(AppModule)
   await app.listen(process.env.PORT || 3000)
 }
